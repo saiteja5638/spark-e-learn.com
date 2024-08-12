012d5d4c692059ee73198b5b5ee12fcd7e4da32d
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-08-12 09:39:33 +0000

    new changes added

--- src/components/pages/Creater.js
+++ src/components/pages/Creater.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import '../../App.css';
 import Navbar from './Navbar';
-
+import Carousel from 'react-bootstrap/Carousel';
 export default function Creater()
 {
     return(
         <>
         <Navbar></Navbar>
-        <h1>Creater</h1>
+        <div className='Creat_M_Conatainer' >
+            <div className='Creat_Carosuel' >
+            <Carousel data-bs-theme="dark" className='Car1' >
+                            <Carousel.Item interval={1000} >
+                                <img
+                                    className='Car'
+                                    src="https://images.pexels.com/photos/5380642/pexels-photo-5380642.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                    alt="First slide"
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item interval={1000} >
+                                <img
+                                    className='Car'
+                                    src="https://images.pexels.com/photos/270488/pexels-photo-270488.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                    alt="Second slide"
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item interval={1000} >
+                                <img
+                                    className='Car'
+                                    src="https://images.pexels.com/photos/3861972/pexels-photo-3861972.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                    alt="Third slide"
+                                />
+                            </Carousel.Item>
+                        </Carousel>
+            </div>
+            <div className='Creat_content'  >
+                 
+            </div>
+        </div>
         </>
     )
 }
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import '../../App.css';
 import Navbar from './Navbar';
-import modelData  from '../models/projects.json';
-import CardItem from '../CardItem';
-import Cards from '../Cards';
-export default function Services()
-{
-    return(
-        <>
-        <Navbar></Navbar>
-        <h1 className='Projects_header_h1' >Only a few projects are displayed. There are many more available.</h1>
-      <div className='Projects_container'>
+import modelData from '../models/projects.json';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+export default function Services() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1 className='Projects_header_h1' >Only a few projects are displayed. There are many more available.</h1>
+      <div className='Projects_container' >
         {modelData.map((item, index) => (
-          <div className='Proj_container_item' key={index}>
-            <CardItem
-              src={item.img1}
-              text={item.title}
-              
-              path={`/view_detail?$id=${item.id}`}
-            />
-          </div>
+
+          <Card sx={{ maxWidth: 345 }} className='Projects_Card'  >
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className='Proj_container_item'
+                height="140"
+                image={item.img1}
+                alt="green iguana"
+              />
+              <CardContent>
+                {/* <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography> */}
+                <Typography variant="subtitle1" color="text.secondary">
+                  {item.title}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         ))}
       </div>
-        </>
-    )
+
+    </>
+  )
 }
